07036f98c35bb60e1b8a45be42304b81cadf0244
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-03-15 11:13:23 -0300

    Add files via upload
    Javascript botão ativo

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = querySelectorAll(".botao");
+
+for(let i= 0; i< botoes.length; i++){
+    botoes[i].onclick = function(){
+        for (j=0; j< botoes.length; j++){
+            botoes[j].classlist.remove("ativo");            
+        }
+        botoes[i].classlist.add("ativo");
+    }
+}
