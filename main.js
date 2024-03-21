32605b459365089f65416b0a0b1b66e901da16be
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-03-20 21:51:25 -0300

    Reparo estrutura do For J

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = querySelectorAll(".botao");
 
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
-        for (j=0; j< botoes.length; j++){
+        for (let j=0; j< botoes.length; j++){
             botoes[j].classlist.remove("ativo");            
         }
         botoes[i].classlist.add("ativo");
