306c79f1c5aef2d29631b7f7e186596e3673f5dd
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-04-05 12:07:32 -0300

    estilizando conteúdo pagina

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const botoes = querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j=0; j< botoes.length; j++){
-            botoes[j].classlist.remove("ativo");            
+            botoes[j].classList.remove("ativo");            
         }
-        botoes[i].classlist.add("ativo");
+        botoes[i].classList.add("ativo");
     }
 }
