98a29663f1b8daffd887a5c91b38b54ee210e358
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-04-12 12:06:41 -0300

    Menu abas conteúdo

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo01 = new Date("2024-10-05T00:00:00");
 
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j=0; j< botoes.length; j++){
-            botoes[j].classList.remove("ativo");            
+            botoes[j].classList.remove("ativo"); 
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
     }
 }
