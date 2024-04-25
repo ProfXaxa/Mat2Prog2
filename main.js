9307c17917f32171d36c6b15118ab309f3efab41
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-04-25 12:11:17 -0300

    timer

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo01 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo02 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo03 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo04 = new Date("2025-02-05T00:00:00");
+let tempoAtual = new Date();
+const tempos =[tempoObjetivo01,tempoObjetivo02,tempoObjetivo03,tempoObjetivo04];
+for (let i = 0; i< contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+
+}
+
+
 
 for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
@@ -13,4 +24,18 @@ for(let i= 0; i< botoes.length; i++){
         textos[i].classList.add("ativo");
 
     }
+}
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal /1000);
+    let minutos = Math.floor(segundos/ 60);
+    let horas = Math.floor(minutos/60);    
+    let dias = Math.floor(horas / 24);
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+
+    return dias + " dias " + horas + " horas " + minutos +" minutos " + segundos + " segundos";
 }
