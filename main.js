5e08a8e7bd51468a52684bb24bd1edbd14142dec
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-05-02 20:02:04 -0300

    Arrumando o Else

--- main.js
+++ main.js
@@ -7,40 +7,39 @@ const tempoObjetivo02 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo03 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo04 = new Date("2025-02-05T00:00:00");
 let tempoAtual = new Date();
-const tempos =[tempoObjetivo01,tempoObjetivo02,tempoObjetivo03,tempoObjetivo04];
-for (let i = 0; i< contadores.length; i++){
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
+for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i]);
 
 }
 
 
 
-for(let i= 0; i< botoes.length; i++){
-    botoes[i].onclick = function(){
-        for (let j=0; j< botoes.length; j++){
-            botoes[j].classList.remove("ativo"); 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
-function calculaTempo(tempoObjetivo){
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal /1000);
-    let minutos = Math.floor(segundos/ 60);
-    let horas = Math.floor(minutos/60);    
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    segundos %=60;
-    minutos %=60;
-    horas %=24;
-    if (tempoFinal >0){
-        return dias + " dias " + horas + " horas " + minutos +" minutos " + segundos + " segundos";
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return[0,0,0,0];
+        return "Prazo Finalizado";
+    }
 
-        }   
-    
 }
