ce54d82078b21af6977291f63dc9c57b9fe5b981
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-05-02 20:14:16 -0300

    função atualiza cronometro

--- main.js
+++ main.js
@@ -40,5 +40,14 @@ function calculaTempo(tempoObjetivo) {
     } else {
         return "Prazo Finalizado";
     }
-
 }
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+comecaCronometro();
