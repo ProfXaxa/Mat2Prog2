adbc895961ffc916f0fe6ed0053b2b49bbec7343
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-06-06 11:15:32 -0300

    tempo

--- main.js
+++ main.js
@@ -31,18 +31,22 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "Prazo Finalizado";
+        return [0.0.0.0];
     }
 }
 function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempo[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempo[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempo[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempo[0])[3];
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        //contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
-//comecaCronometro();
+comecaCronometro();
