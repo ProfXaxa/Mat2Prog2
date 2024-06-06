fd67d506b5ceeb6ac6b2fdb0bb2cb8a4b75070f1
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-06-06 20:39:43 -0300

    arrumanto atualizaCronometro

--- main.js
+++ main.js
@@ -37,10 +37,10 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length;i++){
         contadores[i].textContent = calculaTempo(tempos[i]);
     }
