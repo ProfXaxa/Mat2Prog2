06f1e05bb3342a3470544dc8938d79c6b40002dc
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2024-06-14 11:12:29 -0300

    Finalizado

--- main.js
+++ main.js
@@ -37,11 +37,11 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length;i++){
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
         //contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
